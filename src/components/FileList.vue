10ab383be1b27b9e9a72e47ad64e8652481cc001
fuxuewen <1012155589@qq.com>
2018-10-30 09:33:53 +0800

    补充拖动事件

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="file-list-container">
-      <div class="file-list-content" ref="fileListContent">
+      <div class="file-list-content" ref="fileListContent" @dragenter="dragEnter($event)">
         <div v-if="!selectTreeNode.data.children.length" class="item-file">
           <div class="item-header file">图片文件</div>
           <div class="item-content lev1-file-content">
@@ -13,14 +13,14 @@
         <div :class="{'item-folder': item1.type==1, 'item-file': item1.type !=1}" v-for="(item1, index1) in selectTreeNode.data.children" v-bind:key = "index1">
           <div :class="['item-header', {'folder': item1.type==1, 'file': item1.type==0}]">图片文件</div>
           <div v-if="item1.type==0" class="item-content lev1-file-content" draggable="true"
-            @dragstart="drag($event,item1)"  @drop='drop($event, item1)' @dragover='allowDrop($event, item1)'>
+            @dragstart="dragstart($event,item1)"  @drop='drop($event, item1)' @dragover='dragover($event, item1)'>
             <div>
               <div class="item-content-icon"></div>
               <div class="item-content-text">{{item1.name}}</div>
             </div>
           </div>
           <div v-else class="item-content lev1-folder-content" :style="{width: itemWidth}" v-for="(item2, index2) in item1.children" v-bind:key="index2"
-             @dragstart="drag($event, item2)"  @drop='drop($event, item1)' @dragover='allowDrop($event, item1)'>
+            @dragstart="dragstart($event, item2)"  @drop='drop($event, item1)' @dragover='dragover($event, item1)'>
             <div :class="{'lev2-folder-content': item2.type==1, 'lev2-file-content': item2.type==0}">
               <div class="item-content-icon"></div>
               <div class="item-content-text">{{item2.name}}</div>
@@ -53,6 +53,13 @@ export default class FileList extends Vue {
     children: [],
     type: 0
   }
+  dragTarget: FileObject = {
+    name: '',
+    fullName: '',
+    path: '',
+    children: [],
+    type: 0
+  }
 
   created() {
     // console.log(`created:${this.name}`)
@@ -62,22 +69,49 @@ export default class FileList extends Vue {
     this.$nextTick(function() {})
   }
 
-  drag(event, data): void {
+  // 用户开始拖动元素时触发
+  dragstart(event, data: FileObject): void {
     this.dragData = data
   }
 
-  drop(event, data): void {
+  // 元素正在拖动时触发
+  drag(event) {}
+
+  // 当被鼠标拖动的对象进入其容器范围内时触发此事件
+  dragenter(event) {
+    event.preventDefault()
+  }
+
+  // 当某被拖动的对象在另一对象容器范围内拖动时触发此事件
+  dragover(event, data: FileObject): void {
+    event.preventDefault()
+  }
+
+  // 当被鼠标拖动的对象离开其容器范围内时触发此事件
+  dragleave(event) {}
+
+  //  在一个拖动过程中，释放鼠标键时触发此事件
+  drop(event, data: FileObject): void {
     event.preventDefault()
     if (this.dragData === data) {
       alert('同一元素')
       return
     }
+    // 放下后更新列表数据
+    this.refreshListData(this.dragData, data)
+  }
+
+  // 用户完成元素拖动后触发
+  dragend(event) {}
+
+  // 更新拖动后列表数据
+  refreshListData(dragData: FileObject, targetData: FileObject) {
     const listData: any = this.$props.selectTreeNode.data
-    const index = listData.children.findIndex(item => item === this.dragData)
-    if (data.children.length > 0) {
+    const index = listData.children.findIndex(item => item === dragData)
+    if (targetData.children.length > 0) {
       // 拖动到的对象是文件夹
       if (index >= 0) {
-        data.children.push(this.dragData)
+        targetData.children.push(dragData)
         listData.children.splice(index, 1)
       }
     } else {
@@ -85,21 +119,17 @@ export default class FileList extends Vue {
       if (index >= 0) {
         listData.children.splice(index, 1)
         const newFolder: FileObject = {
-          name: `${data.name}_${this.dragData.name}`,
-          fullName: `${data.path}\\${data.name}_${this.dragData.name}`,
-          path: data.path,
+          name: `${targetData.name}_${dragData.name}`,
+          fullName: `${targetData.path}\\${targetData.name}_${dragData.name}`,
+          path: targetData.path,
           type: 1,
           children: []
         }
         newFolder.children.push(this.dragData)
-        newFolder.children.push(data)
+        newFolder.children.push(targetData)
         listData.children.push(newFolder)
       }
     }
-  }
-
-  allowDrop(event, data): void {
-    event.preventDefault()
   }
 }
 </script>
